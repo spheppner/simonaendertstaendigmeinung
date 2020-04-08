825b1cd5ff112245b0e6edc65dd4396dd5f246ae
Simon Heppner <20207210+spheppner@users.noreply.github.com>
2020-04-08 16:14:03 +0200

    Update storygenerator.py

--- storygenerator.py
+++ storygenerator.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-## TODO: Evilness, Morality, Death
+## TODO: Divorce
 
 class World:
     persons = {}
@@ -114,22 +114,29 @@ def story():
     feed_list_from_file(World.last_names, "surnames.txt")
     feed_list_from_file(   World.first_names_male, "male_firstnames.txt")
     feed_list_from_file(World.first_names_female, "female_firstnames.txt")
-    print(World.last_names)
-
     
-    for nr in range(10):
-        x = Person(min_age=20, max_age=25, year=2020, gender="male" if nr % 2 == 0 else "female")  ## adam und eva *2.5
-    for year in range(1921, 2099):
+    for nr in range(15):
+        x = Person(min_age=0, max_age=25, year=2000, gender="male" if nr % 2 == 0 else "female")  ## adam und eva *2.5
+    for year in range(2000, 2100):
         # for n in World.persons.values()
+        
         for n in [p for p in World.persons.values()]:
             n.update(year)
-        for n in World.persons.values():
+        for n in [p for p in World.persons.values()]:
             if n.update_year is None or n.update_year != year:
                 n.update(year)
-    for n in World.persons.values():
-        n.print_history()
-
-
+        print(year, len([p for p in World.persons.values() if p.death_year is None]), len([p for p in World.persons.values() if p.death_year is None and p.married is not None]))
+    
+    #names = {}
+    #for n in World.persons.values():
+    #    if n.last_name in names:
+    #        names[n.last_name] += 1
+    #    else:
+    #        names[n.last_name] = 1
+    #print(names)
+    
+    #    n.print_history()
+    
 class Person:
     number = 0
 
@@ -142,11 +149,19 @@ class Person:
         self.last_name = last_name if last_name is not None else random.choice(World.last_names)
         self.gender = random.choice(("male", "female")) if gender is None else gender
         self.first_name = random.choice(World.first_names_male if self.gender == "male" else World.first_names_female)
+        self.probability_of_death = 0.0001
+        self.moral = random.randint(0,1) * 0.6 + 0.4
+        self.situation = random.randint(5, 10)
         
-        self.probability_of_death = 0.001
         self.death_year = None
-        # self.year = year
         self.birth_year = year - random.randint(min_age, max_age)
+        self.marriage_year = None
+        
+        if self.birth_year < year:
+            age = year-self.birth_year
+            for i in range(age):
+                if i % 8 == 0:
+                    self.probability_of_death *= 2
 
         # self.place_of_birth = random.choice(("Vienna", "Munich", "Berlin"))
         self.youth = random.choice(("student", "worker", "unemployed"))
@@ -166,58 +181,112 @@ class Person:
         # self.get_children()
         self.update_year = None
 
+    #def update(self, year):
+    #    if self.death_year is not None:
+    #        return(self.number) + "====== "+ "arriving in city" if immigrant else "born here" 
+    #    self.history = [(text + " with those attributes:" + str(self.__dict__))]
+    #    # self.get_children()
+    #    self.update_year = None
+
     def update(self, year):
         if self.death_year is not None:
             return
-        # double death probability every 8 years
+            
+        # ----- double death probability every 8 years -----
         age = year-self.birth_year
         if age % 8 == 0:
             self.probability_of_death *= 2
         
-        text = "year {}: ".format(year)
-        if self.married is None and age > 18:
-            # chance to find partner
-            if random.random() < 0.8:
+        # ----- trying to marry someone -----
+        if self.married is None and age > 16:
+            if random.random() < 0.9:
                 spouse = self.get_married(year)
-                if spouse is None:
-                    text += "tried very hard but failed to marry someone"
-                else:
-                    text += "suscessfully married to {}!".format(self.married)
-        # chance to find new friend
-        if random.random() < 0.2:
+                
+        # ----- chance to find new friend -----
+        if random.random() < 0.3:
             newfriend = self.get_friends(year)
-            if newfriend is None:
-                text += "tried to start a friendship but failed."
-            else:
-                text += " Becomes friend with {}.".format(newfriend)
 
-        # chance to loose friend
+        # ----- chance to loose friend -----
         if len(self.friends) > 0 and random.random() < 0.05:
             exfriend = random.choice(self.friends)
             self.friends.remove(exfriend)
-            ## TODO becomes foe ?
-            text += " Ends friendship with {}.".format(exfriend)
-
+            self.history.append("{}: Ends friendship with {}.".format(year, exfriend))
             World.persons[exfriend].friends.remove(self.number)
-            World.persons[exfriend].history.append("{}: Looses friendship with {}".format(year, self.number))
+            World.persons[exfriend].history.append("{}: Looses friendship with {}.".format(year, self.number))
 
-        # chance to get baby
+        # ----- chance to get baby -----
         self.get_children(year)
-
-        self.history.append(text)
-        self.update_year = year
+        
+        # ----- changing of situation -----
+        self.situation += random.randint(-2, 3)
+        if self.situation < 0:
+            self.moral *= 0.85
+        else:
+            self.moral *= 1.1
+        
+        # ----- crime event -----
+        if self.situation < 0:
+            p1 = abs(self.situation) * 0.01
+            if random.random() < p1:
+                # bad situation creates chance for crime
+                if random.random() > self.moral:
+                    crime = random.choice(("destruction_friendship", "cheat_marriage", "stealing", "killing"))
+                    if crime == "destruction_friendship":
+                        nx = random.choice(list(World.persons.keys()))
+                        x = World.persons[nx]
+                        if x.friends != []:
+                            y = x.friends[0]
+                            World.persons[nx].friends.remove(y)
+                            World.persons[y].friends.remove(nx)
+                            self.history.append("{}: {} destroyed the friendship of {} and {}".format(year, self.number, nx, y))
+                            World.persons[nx].history.append("{}: {} destroyed the friendship of {} and {}".format(year, self.number, nx, y))
+                            World.persons[y].history.append("{}: {} destroyed the friendship of {} and {}".format(year, self.number, nx, y))
+                            
+                    elif crime == "cheat_marriage":
+                        nx = random.choice(list(World.persons.keys()))
+                        x = World.persons[nx]
+                        if x.married is not None:
+                            y = x.married
+                            World.persons[nx].married = None
+                            World.persons[y].married = None
+                            self.history.append("{}: Destroyed the marriage of {} and {}".format(year, nx, y))
+                            World.persons[nx].history.append("{}: {} destroyed the marriage of {} and {}".format(year, self.number, nx, y))
+                            World.persons[y].history.append("{}: {} destroyed the marriage of {} and {}".format(year, self.number, nx, y))
+                    elif crime == "stealing":
+                        nx = random.choice(list(World.persons.keys()))
+                        x = World.persons[nx]
+                        x.situation -= random.randint(10,20)
+                    elif crime == "killing":
+                        ## TODO extra check
+                        nx = random.choice(list(World.persons.keys()))
+                        x = World.persons[nx]
+                        x.history.append("{}: Got killed by {}.".format(year, self.number))
+                        x.death_year = year
+                        if self.married is not None:
+                            World.persons[x.married].married = None
+                        for friend in x.friends:
+                            World.persons[friend].friends.remove(nx)
+                    print(crime, self.number, year, self.situation, self.moral)          
+        
+        # ----- chance to die -----
         if random.random() < self.probability_of_death:
             self.history.append("{}: Died of natural causes.".format(year))
             self.death_year = year
-
+            if self.married is not None:
+                World.persons[self.married].married = None
+            for friend in self.friends:
+                World.persons[friend].friends.remove(self.number)
+                
     def get_friends(self, year):
         """friends must have: different number, age-difference +- 10 at max. """ 
         candidates = [p for p in World.persons.values() if p.death_year is None and p.number != self.number and abs(
             p.birth_year - self.birth_year) < 20 and p.number not in self.friends and p.number not in self.foes and p.number not in self.children and p.number != self.married]# and self.birth_year < year - 5]
         if len(candidates) == 0:
+            self.history.append("{}: Found no suitable people to befriend.".format(year))
             return None
         x = random.choice(candidates).number
         self.friends.append(x)
+        self.history.append("{}: Found a new friend: {}".format(year, x))
         # gegenseitige Freundschaft:
         World.persons[x].friends.append(self.number)
         World.persons[x].history.append("{}: Accepted friendship request of {} ".format(year, self.number))
@@ -226,44 +295,54 @@ class Person:
     def get_married(self, year):
         """married people must have: different number, age-difference +- 10 at max. """
 
-        candidates = [p for p in World.persons.values() if p.death_year is None and p.married is None and p.number != self.number and abs(
-            p.birth_year - self.birth_year) < 16 and p.number not in self.foes and p.number not in self.children and p.gender != self.gender and p.birth_year < year - 20]
+        candidates = [p for p in World.persons.values() if p.death_year is None and p.married is None and p.number != self.number and p.number not in self.foes and p.number not in self.children and p.gender != self.gender and p.birth_year < year - 16 and not p.is_sibling(self)]
         if len(candidates) == 0:
+            self.history.append("{}: Found no suiteable candidates to marry.".format(year))
             return None
         x = random.choice(candidates).number
         self.married = x
         # take name of partner
         self.last_name = World.persons[x].last_name
         # remove spouse of friendlist to avoid becoming ex-friend
+        self.history.append("{}: Managed to marry {} and changed last name to {}.".format(year, self.married, self.last_name))
         if x in self.friends:
             self.friends.remove(x)
         if self.number in World.persons[x].friends:
             World.persons[x].friends.remove(self.number)
         # self.marriages.add(x)
         # gegenseitige Heirat:
+        self.marriage_year = year
+        World.persons[x].marriage_year = year
         World.persons[x].married = self.number
-        World.persons[x].history.append("{}: Gracefully accepted marriage proposal of {} ".format(year, self.number))
+        World.persons[x].history.append("{}: Gracefully accepted marriage proposal of {}.".format(year, self.number))
         # little chance that both choose a comlete new name
-        if random.random() < 0.15:
+        if random.random() < 0.4:
             new_name = random.choice(World.last_names)
             self.last_name = new_name
             World.persons[x].last_name = new_name
-            self.history.append("{}: The married couple chose the new family name {}".format(year, new_name))
+            World.persons[x].history.append("{}: The married couple chose the new family name {}.".format(year, new_name))
+            self.history.append("{}: The married couple chose the new family name {}.".format(year, new_name))
         return x
 
     def get_children(self, year):
         """children only from married couple (including ex), children for both parents! children at least 20 years younger"""
-
-        if self.married is not None:
-            children = len(self.children)
-            p = 1 / (3 + children)
-            if random.random() < p:
-                # NEUER MENSCH BABABABY
-                baby = Person(year, min_age=0, max_age=0, last_name=self.last_name, immigrant=False)
-                self.children.append(baby.number)
-                World.persons[self.married].children.append(baby.number)
-                self.history.append("{}: Got a baby: {}".format(year, baby.number))
-                World.persons[self.married].history.append("{}: Got a baby: {}".format(year, baby.number))
+        if self.married is None:
+            return
+        if self.gender != "female":
+            return
+        if year-self.marriage_year < 1:
+            return
+        if year-self.birth_year > 50:
+            return
+        children = len(self.children)
+        p = 1 / (3 + children)
+        if random.random() < p:
+            # ----- Baby kriegen -----
+            baby = Person(year, min_age=0, max_age=0, last_name=self.last_name, immigrant=False)
+            self.children.append(baby.number)
+            World.persons[self.married].children.append(baby.number)
+            self.history.append("{}: Mommy got a baby: {}.".format(year, baby.number))
+            World.persons[self.married].history.append("{}: Becomes daddy of: {}.".format(year, baby.number))
 
         # if len(self.marriages) == 0:
         #    return
@@ -273,6 +352,21 @@ class Person:
 
         #    x = random.choice(candidates).number
         #    self.children.add(x)
+        
+    def get_parents(self):
+        parents = []
+        for p in [p for p in World.persons.values() if p.birth_year < self.birth_year - 15]:
+            if self.number in p.children:
+                parents.append(p.number)
+        return parents
+        
+    def is_sibling(self, x):
+        """ Check if person x shares at least one parent with person self"""
+        for p in self.get_parents():
+            if p in x.get_parents():
+                return True
+        return False
+        
     def print_history(self):
         for line in self.history:
             print(line)
